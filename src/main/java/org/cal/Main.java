b4661611483eb2fad836eaf06da9d9aad6351de2
Hyjacker-1 <Heet.Vasani@iiitb.org>
2023-10-28 15:44:26 +0530

    Hey this is my first commit

---
+++ src/main/java/org/cal/Main.java
@@ -0,0 +1,11 @@
+package org.cal;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is a calculator program");
+    }
+}
